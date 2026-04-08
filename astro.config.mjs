9a8ec85cbe3c80ff51f69aa6bd8fc368928d362c
Irvan Fauziansyah <ervhan@gmail.com>
2026-04-08 22:13:35 +0700

    feat: integrate @astrojs/sitemap to generate and link site index map

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://mycodingplan.com',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
